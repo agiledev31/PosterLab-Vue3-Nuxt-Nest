69ded1dc37e817810320937d032db4a354c40f15
vardym <talentdev725@gmail.com>
2022-06-02 00:12:04 +0800

    update

---
+++ nest-backend/src/mail/dto/newsletter.dto.ts
@@ -0,0 +1,6 @@
+import { IsEmail } from 'class-validator'
+
+export class NewsletterDto {
+    @IsEmail()
+    email: string
+}
---
+++ nest-backend/src/mail/dto/send-message.dto.ts
@@ -0,0 +1,9 @@
+import { PickType } from '@nestjs/swagger'
+import { User } from '../../iam/users/entities/user.entity'
+
+export class SendMessageUserRequestDto extends PickType(User, [
+    'firstName',
+    'lastName',
+    'email',
+    'organizationName'
+]) {}

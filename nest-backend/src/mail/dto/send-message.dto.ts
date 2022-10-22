167b611dcd005b42861aff7fba71f55dd55234c5
greatadel <115809316+greatadel@users.noreply.github.com>
2022-10-22 12:56:27 +0000

    Update send-message.dto.ts

--- nest-backend/src/mail/dto/send-message.dto.ts
+++ nest-backend/src/mail/dto/send-message.dto.ts
@@ -1,6 +1,5 @@
 import { PickType } from '@nestjs/swagger'
 import { User } from '../../iam/users/entities/user.entity'
-
 export class SendMessageUserRequestDto extends PickType(User, [
     'firstName',
     'lastName',
